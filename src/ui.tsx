f3171db011bc76d436177a4fb97facc0d3e5c310
Vinicius Pretto <vini.prettodasilva@gmail.com>
2020-09-10 20:20:57 -0300

    Add project structure

---
+++ src/ui.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./ui.css";
+
+class App extends React.Component {
+  render() {
+    return <h2>Tokens</h2>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("react-page"));
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebpackInlineSourcePlugin = require("html-webpack-inline-source-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = (env, argv) => ({
+  mode: argv.mode === "production" ? "production" : "development",
+  // This is necessary because Figma's 'eval' works differently than normal eval
+  devtool: argv.mode === "production" ? false : "inline-source-map",
+  entry: {
+    ui: "./src/ui.tsx",
+    code: "./src/code.ts",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        loader: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|webp|svg|zip)$/,
+        loader: [
+          {
+            loader: "url-loader",
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/ui.html",
+      filename: "ui.html",
+      inlineSource: ".(js)$",
+      chunks: ["ui"],
+    }),
+    new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin),
+  ],
+});
