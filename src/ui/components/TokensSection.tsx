e4b15500247f17783dd96535f29c3f816a4c06d2
Vinicius Pretto <vini.prettodasilva@gmail.com>
2020-11-02 18:11:27 -0300

    Add copy button

--- src/ui/components/TokensSection.tsx
+++ src/ui/components/TokensSection.tsx
@@ -1,9 +1,26 @@
 import * as React from "react";
 
 const TokensSection = ({ tokens }) => {
+  const copyText = () => {
+    const copyText = document.querySelector("#tokens").textContent;
+    const textArea = document.createElement("textarea");
+    textArea.value = copyText;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+  };
+
   return (
     <div className="p-md">
-      <pre className="highlight">{tokens}</pre>
+      <div className="highlight">
+        <div className="d-flex flex-row justify-content-end mb-sm">
+          <button className="button button--primary" onClick={copyText}>
+            copy
+          </button>
+        </div>
+        <pre id="tokens">{tokens}</pre>
+      </div>
     </div>
   );
 };
